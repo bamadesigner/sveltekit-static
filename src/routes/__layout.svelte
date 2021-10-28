241fe5fd00f73e65e207d3c9c6cbc820361e1439
Rachel Cherry <bamadesigner@gmail.com>
2021-10-28 16:42:19 -0400

    Adding Skip to main link to layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,4 +2,27 @@
 	import '$lib/css/dist.css';
 </script>
 
+<a id="skip-to-main" href="#main" class="sr-only">Skip to main content</a>
+
 <slot />
+
+<style type="text/postcss">
+	#skip-to-main:focus,
+	#skip-to-main:hover {
+		@apply bg-blue-800;
+		font-size: 1.2rem;
+		line-height: normal;
+		color: #fff;
+		width: auto;
+		height: auto;
+		left: 0;
+		top: 0;
+		clip: unset;
+		padding: 1rem;
+		margin: 0;
+		overflow: auto;
+		white-space: normal;
+		border: 0;
+		z-index: 1000;
+	}
+</style>
